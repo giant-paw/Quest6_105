b556a9e7486101f7bec11330730221080f7d39a9
giant-paw <giantpawbusiness@gmail.com>
2024-11-20 18:49:15 +0700

    membuat bagian box bawah yang bewarna putih dan mempunyai garis lengkung

--- app/src/main/java/com/example/pam_20nov/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pam_20nov/ui/view/screen/RencanaStudyView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -62,6 +63,17 @@ fun RencanaStudyView(
                     fontWeight = FontWeight.Bold
                 )
             }
+        }
+
+        Box(modifier = Modifier
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(
+                    topEnd = 15.dp,
+                    topStart = 15.dp
+                )
+            )
+        ){
 
         }
     }
