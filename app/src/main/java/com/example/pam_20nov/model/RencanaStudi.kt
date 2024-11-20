52d51a09078d122be2fef59bf25e650482f036ad
giant-paw <giantpawbusiness@gmail.com>
2024-11-20 20:53:11 +0700

    membuat fungsi di viewModel

---
+++ app/src/main/java/com/example/pam_20nov/model/RencanaStudi.kt
@@ -0,0 +1,6 @@
+package com.example.pam_20nov.model
+
+data class RencanaStudi(
+    val namaMK: String = "",
+    val kelas: String = ""
+)
---
+++ app/src/main/java/com/example/pam_20nov/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.pam_20nov.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pam_20nov.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel : ViewModel(){
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMk -> stateMk.copy(namaMK = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String) {
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
+
+
+}
