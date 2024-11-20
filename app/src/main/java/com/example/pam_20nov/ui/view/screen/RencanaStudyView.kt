3159a1350db76c0eba768dd34b58f427708e3c98
giant-paw <giantpawbusiness@gmail.com>
2024-11-20 18:36:48 +0700

    membuat header yang berisi image

--- app/src/main/java/com/example/pam_20nov/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pam_20nov/ui/view/screen/RencanaStudyView.kt
@@ -1,10 +1,48 @@
 package com.example.pam_20nov.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pam_20nov.R
 
 @Preview(showBackground = true)
 @Composable
-fun RencanaStudyView(){
+fun RencanaStudyView(
+){
 
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ){
+        Row (modifier = Modifier
+            .padding(40.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ){
+            Image(
+                painter = painterResource(
+                    id = R.drawable.umy
+                ),
+                contentDescription = ""
+            )
+
+            Spacer(modifier = Modifier.padding(start = 30.dp))
+
+        }
+    }
 }
