4a79b67ba41f85e6003de414fc09eb8d5d153bd3
giant-paw <giantpawbusiness@gmail.com>
2024-11-20 18:31:41 +0700

    Menambahkan file untuk halaman pilih study dan menampilkan data yang sudah di input

---
+++ app/src/main/java/com/example/pam_20nov/ui/view/screen/RencanaStudyView.kt
@@ -0,0 +1,10 @@
+package com.example.pam_20nov.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun RencanaStudyView(){
+
+}
