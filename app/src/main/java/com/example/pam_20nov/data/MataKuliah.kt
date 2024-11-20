2def4ab58763dfdf01a38a2444254d29a57b6e2a
giant-paw <giantpawbusiness@gmail.com>
2024-11-20 17:27:13 +0700

    disini kita menggunakan list yang menampung 5 matakuliah

--- app/src/main/java/com/example/pam_20nov/data/MataKuliah.kt
+++ app/src/main/java/com/example/pam_20nov/data/MataKuliah.kt
@@ -2,6 +2,10 @@ package com.example.pam_20nov.data
 
 object MataKuliah {
     val options = listOf(
-
+        "Computer Vision",
+        "Pengembangan Aplikasi Web",
+        "Aplikasi Multimedia",
+        "Logika Teknik Pemrograman",
+        "Serious Game"
     )
 }
